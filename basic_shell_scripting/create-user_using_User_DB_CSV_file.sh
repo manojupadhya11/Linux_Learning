8eb5e90ec255433d66c7e9378947067456979a0b
manojupadhya11 <manojupadhya11@gmail.com>
2024-09-10 10:41:23 +0530

    Shell Script for creating user using CSV file

---
+++ basic_shell_scripting/create-user_using_User_DB_CSV_file.sh
@@ -0,0 +1,21 @@
+#/usr/bin/bash
+
+#user=$1
+
+users=$(cat user-db.csv)
+
+for j in $users
+do
+ if id $j &> /dev/null
+ then
+         echo " $j is already present"
+ else
+        useradd $j &> /dev/null
+        if [ 0 -eq $? ]
+        then
+                echo "user $j is been created succesfully"
+        else
+                echo "user creation failed, server error"
+fi
+ fi
+done
